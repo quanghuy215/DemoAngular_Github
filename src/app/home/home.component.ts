233a497246f915668c7ff8154b6eaf6c02a614d6
Harry Nguyen <nguyenquanghuy2021@gmail.com>
2022-04-07 10:30:00 +0700

    practice ngFor with Obiect

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,14 @@ export class HomeComponent implements OnInit {
   public name = `Harry`;
   public age = 25;
   public fruit =['Apple','Orange','Watermelon','Mango','Durian','Banana'];
+  public fruit2 =[
+    {name:'Apple',price:21,sale:true},
+    {name:'Orange',price:43,sale:false},
+    {name:'Watermelon',price:15,sale:true},
+    {name:'Mango',price:73,sale:false},
+    {name:'Durian',price:64,sale:true},
+    {name:'Banana',price:23,sale:false},
+  ];
 
   constructor() { }
 
