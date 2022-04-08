f1fc8ab301ae8da2dca5afa7e6347a1005bce14f
Harry Nguyen <nguyenquanghuy2021@gmail.com>
2022-04-08 10:02:31 +0700

    practice with attribute directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AboutComponent } from './about/about.component';
 import { FormsModule } from '@angular/forms';
 import { OnSalePipe } from './on-sale.pipe';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HighLightDirective } from './Directives/high-light.directive';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
     OnSalePipe,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HighLightDirective
   ],
   imports: [
     BrowserModule,
