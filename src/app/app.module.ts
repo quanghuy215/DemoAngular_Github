7db59c2ca94c5553f5b663132a7d1936406818e2
Harry Nguyen <nguyenquanghuy2021@gmail.com>
2022-04-08 08:51:14 +0700

    practice with router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AboutComponent } from './about/about.component';
 
 import { FormsModule } from '@angular/forms';
 import { OnSalePipe } from './on-sale.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    OnSalePipe
+    OnSalePipe,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
